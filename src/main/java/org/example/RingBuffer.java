830d084dca621132e20358bde63ec415d6d206fb
Arina Zubova <arina777pototskaya@gmail.com>
2025-10-18 21:30:08 +0300

    refactoring

--- src/main/java/org/example/RingBuffer.java
+++ src/main/java/org/example/RingBuffer.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class RingBuffer<T> {
     private final T[] buffer;
     private final int capacity;
@@ -8,6 +10,9 @@ public class RingBuffer<T> {
     private int currentSize;
 
     public RingBuffer(int capacity) {
+        if (capacity < 1) {
+            throw new IllegalArgumentException("Capacity must be grater than 0");
+        }
         this.buffer = (T[]) new Object[capacity];
         this.capacity = capacity;
     }
@@ -29,7 +34,18 @@ public class RingBuffer<T> {
         return value;
     }
 
-    private boolean isEmpty() {
+    public synchronized boolean isFull() {
+        return currentSize == capacity;
+    }
+
+    public synchronized boolean isEmpty() {
         return currentSize == 0;
+    }
+
+    public synchronized void close() {
+        Arrays.fill(buffer, null);
+        tail = 0;
+        head = 0;
+        currentSize = 0;
     }
 }
